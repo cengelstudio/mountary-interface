8ab0b8001d6e7d97cf3b5613ca095128b69b79b1
metehansaral <metehansaral@gmail.com>
2025-05-28 06:30:21 +0300

    some text changed

--- backend/routes.py
+++ backend/routes.py
@@ -20,7 +20,7 @@ def render_build_pending():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Track Interface - Build Bekleniyor</title>
+        <title>Mountary</title>
         <style>
             * {
                 margin: 0;
